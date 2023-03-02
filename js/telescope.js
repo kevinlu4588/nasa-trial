ffe22aee5b8cf208c21680c5dd4b3a52757a6484
kevinlu4588 <kevinlu4588@gmail.com>
2023-03-02 17:16:06 -0500

    im so stupid

--- js/telescope.js
+++ js/telescope.js
@@ -1,7 +1,15 @@
 //Adapted from https://pixijs.io/examples/#/masks/filter.js
 
-const app = new PIXI.Application();
+const app = new PIXI.Application({
+    width: window.innerWidth - 500,
+    height: window.innerHeight,
+    backgroundColor: 0x2980b9
+}
+);
+
 document.getElementById("telescope").appendChild(app.view);
+const container = new PIXI.Container();
+app.stage.addChild(container);
 
 // Inner radius of the circle
 const radius = 100;
@@ -11,15 +19,41 @@ const blurSize = 32;
 
 var imgURL;
 $.ajax({
-    url:"https://api.nasa.gov/planetary/apod?api_key=jVQ018aAXegqAo7CE1WxklvjiHlpcswlQoHB3piO",
+    url:"https://api.nasa.gov/planetary/apod?api_key=jVQ018aAXegqAo7CE1WxklvjiHlpcswlQoHB3piO&count=5",
     success:function(returnJSON) {
-
-        PIXI.Assets.load("https://cors.office.dataculturegroup.org/" + returnJSON.url).then((grassTexture) => {
-            const background = new PIXI.Sprite(grassTexture);
-            app.stage.addChild(background);
-            background.width = app.screen.width;
-            background.height = app.screen.height;
+        var imgArr = [];
+        for(var i = 0; i < 5; i += 1){
+            console.log("prefi" + i);
+            console.log(returnJSON[i].url);
+            var xcoord = i*40;
+            console.log(xcoord);
+            var ycoord = 400;
+            
+            PIXI.Assets.load("https://cors.office.dataculturegroup.org/" + returnJSON[i].url).then((APOD1) => {
+                const bunny = new PIXI.Sprite(APOD1);
+                bunny.anchor.set(0.5);
+                bunny.x = Math.random() * (1000 - 100) + 100;
+                bunny.y =  Math.random() * (1000 - 100) + 100;
+                var widthRatio = 200/bunny.width;
+                bunny.width = bunny.width * widthRatio;
+                bunny.height = bunny.height*widthRatio;
+                console.log(bunny.x);
+                app.stage.addChild(bunny);
+            });
+        }
+        console.log(imgArr);
+        container.x = app.screen.width / 2;
+        container.y = app.screen.height / 2;
         
+        
+        
+       }
+});
+console.log(imgURL);
+
+//const texture = PIXI.Texture.from(imgURL);
+
+            /*
             const circle = new PIXI.Graphics()
                 .beginFill(0xFF0000)
                 .drawCircle(radius + blurSize, radius + blurSize, radius)
@@ -39,11 +73,4 @@ $.ajax({
                 focus.position.x = event.global.x - focus.width / 2;
                 focus.position.y = event.global.y - focus.height / 2;
             });
-        });
-        
-       }
-});
-console.log(imgURL);
-
-//const texture = PIXI.Texture.from(imgURL);
-
+            */
