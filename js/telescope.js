6d9ef3e6cf5d84f7da268d30a438772591dcaae1
kevinlu4588 <kevinlu4588@gmail.com>
2023-03-02 21:53:44 -0500

    got some stuff to work

--- js/telescope.js
+++ js/telescope.js
@@ -3,13 +3,88 @@
 const app = new PIXI.Application({
     width: window.innerWidth - 500,
     height: window.innerHeight,
-    backgroundColor: 0x2980b9
 }
 );
+function random(max) {
+    return Math.floor(Math.random() * max);
+  }
+var displayWidth = app.width;
+var displayHeight = app.height;
 
 document.getElementById("telescope").appendChild(app.view);
 const container = new PIXI.Container();
 app.stage.addChild(container);
+
+let stars = [];
+
+let nStars = 120;
+
+    for (let i = 0; i < nStars; i++ ) {
+
+        let color;
+        let radius;
+
+        if (i%9 === 0) {
+            color = "#1D299F"
+        } else if (i%19 === 0) {
+            color = "#98312C"
+        } else {
+            color = "#FFFFFF"
+        }
+
+        if (i%12 === 0) {
+            radius = 2;
+        } else {
+            radius = 1;
+        }
+        let obj = new PIXI.Graphics();
+        obj.beginFill(0xFFFFFF);
+        const texture = PIXI.Texture.from('/assets/star.png');
+        var starx = Math.random() * (1000 - 100) + 100;
+        var stary =  Math.random() * (1000 - 100) + 100;
+        obj.drawRect(starx, stary, 2, 2);
+        obj.alpha = 0.5;
+
+// designate circle as being interactive so it handles events
+obj.interactive = true;
+
+// create hit area, needed for interactivity
+obj.hitArea = new PIXI.Circle(150, 150, 100);
+
+// make circle non-transparent when mouse is over it
+obj.mouseover = function(mouseData) {
+  this.alpha = 1;
+}
+
+// make circle half-transparent when mouse leaves
+obj.mouseout = function(mouseData) {
+  this.alpha = 0.5;
+}
+obj.click = function(mouseData){
+    console.log("clicked");
+}
+        console.log(obj);
+        app.stage.addChild(obj);
+    }
+    console.log(stars)
+
+function draw() {
+
+    background(0);
+    for (let i = 0; i < nTwinkle; i++ ) {
+        stroke(data[i].color);
+        rect(data[i].x, data[i].y, data[i].radius, data[i].radius)
+    }
+}
+
+
+
+
+
+
+
+
+
 
 // Inner radius of the circle
 const radius = 100;
@@ -51,11 +126,10 @@ $.ajax({
 });
 console.log(imgURL);
 
-//const texture = PIXI.Texture.from(imgURL);
 
-            /*
+            
             const circle = new PIXI.Graphics()
-                .beginFill(0xFF0000)
+                .beginFill(0xFFFFFF)
                 .drawCircle(radius + blurSize, radius + blurSize, radius)
                 .endFill();
             circle.filters = [new PIXI.filters.BlurFilter(blurSize)];
@@ -65,7 +139,7 @@ console.log(imgURL);
             const focus = new PIXI.Sprite(texture);
         
             app.stage.addChild(focus);
-            background.mask = focus;
+            app.mask = focus;
         
             app.stage.interactive = true;
             app.stage.hitArea = app.screen;
@@ -73,4 +147,4 @@ console.log(imgURL);
                 focus.position.x = event.global.x - focus.width / 2;
                 focus.position.y = event.global.y - focus.height / 2;
             });
-            */
+            
