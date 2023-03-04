e8afbfb11242c79823856d9d65dcffd7cae1db02
Joli Holmes <holmesjoli@gmail.com>
2023-03-04 05:53:44 -0500

    added dropdown options

--- js/nasaImageApi.js
+++ js/nasaImageApi.js
@@ -89,9 +89,23 @@ const constellations = [
     {"abbr": "lac", "greekName": "Lacerta", "commonName": "the Lizard"}
 ];
 
+d3.selectAll(".dropdown-option").on("click", function(d) {
+    let dropdown = d3.select(this).property("value");
+    console.log(dropdown)
+})
+
+const returnConstOpions = () => {
+
+    let opts = constellations
+      .map((item, index) => (
+        `<option class="dropdown-option" value=${item.abbr}> ${item.commonName} (${item.greekName})</option>`
+    ));
+    document.getElementById("constellation").innerHTML = opts;
+};
+
+
 // var applicationId = 'd9c326dc-833b-474f-b51a-f157d89954ce';
 // var applicationSecret = '8de6ffd9d55939a76d782dc6dfb19998a457ee836f6e89af0f4a21c865f1ee13c00135dba55d5b86ea2e4446a1ddaf844ef611deb0f3b00b813297d02f8e447f37cb745b24cb2939b45c8cbbcc6e98dc53bb380ef8b0800a5c1200d39dda017b15fcdfe7c1fe715cad76da2260947c1c';  
-
 
 const apiId = "f6db66df-5ad0-4c42-b153-060bc2bd2de0";
 const secret = "eaab42a1965d4ae799de1d87a7f98638d8c31a72132eb88cc07e106a73ff48ea622c469f90c7cb5f73b669de2fecdb886e19529dda03b632cd7a5d560e05dd37b6c407bc646e445d71e80b9cbad78360e2a3c7d3aeb47018cbfdf0b26b43c3bbe1cbcef979d6d683b4679bd2d3b9d0e2";
@@ -129,4 +143,6 @@ function fetchStarChart() {
     // img.src = data.;
 }
 
+returnConstOpions();
 // fetchStarChart();
+
