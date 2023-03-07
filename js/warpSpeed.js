b25c20ec76553dea123f2e9107bae84f431ad9f2
Joli Holmes <holmesjoli@gmail.com>
2023-03-07 11:33:21 -0500

    moved js to a separate file not in HTML

---
+++ js/warpSpeed.js
@@ -0,0 +1,8 @@
+var warp = new WarpSpeed("canvas3",
+    '{"speed":1,"targetSpeed": 20, "speedAdjFactor":0.03,"density":0.8,"shape":"circle","warpEffect":true,"warpEffectLength":2,"depthFade":true,"starSize":2,"backgroundColor":"#000000","starColor":"#FFFFFF"}'
+    );
+    //    console.log("warp" + console.log(JSON.stringify(warp, null, 4)));
+    setTimeout(() => { 
+    //clearCanvas();             
+    }, 3000
+);
