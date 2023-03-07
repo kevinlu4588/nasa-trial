bd5ca6e1865d941e7376f755bb15f59fe05660c5
Joli Holmes <holmesjoli@gmail.com>
2023-03-07 11:54:34 -0500

    simplified html

--- js/warpSpeed.js
+++ js/warpSpeed.js
@@ -1,4 +1,4 @@
-var warp = new WarpSpeed("canvas3",
+var warp = new WarpSpeed("intro-canvas",
     '{"speed":1,"targetSpeed": 20, "speedAdjFactor":0.03,"density":0.8,"shape":"circle","warpEffect":true,"warpEffectLength":2,"depthFade":true,"starSize":2,"backgroundColor":"#000000","starColor":"#FFFFFF"}'
     );
     //    console.log("warp" + console.log(JSON.stringify(warp, null, 4)));
