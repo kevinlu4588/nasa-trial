520b8f1c3d968d0abe04c61d0a700fac400291ab
Joli Holmes <holmesjoli@gmail.com>
2023-03-07 11:08:41 -0500

    formated css

---
+++ js/ipApi.js
@@ -0,0 +1,12 @@
+// https://ipgeolocation.abstractapi.com/v1/?api_key=f8c1e7effe5747349417b09fa0afe091
+
+
+// Star chart API
+function fetchIP() {
+    const url = "https://ipgeolocation.abstractapi.com/v1/?api_key=f8c1e7effe5747349417b09fa0afe091";
+    fetch(url, {method: "GET"}).
+    then(response => response.json()).
+    then(data => console.log(data.city, data.region))
+}
+
+fetchIP();
