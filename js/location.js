7d335529630b655a7f0f69c4e7b464dfd46d73fe
Joli Holmes <holmesjoli@gmail.com>
2023-03-07 11:24:12 -0500

    simplified code

--- js/location.js
+++ js/location.js
@@ -1,20 +1,18 @@
-var x = document.getElementById("demo");
 let latitude, longitude;
 
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    x.innerText = "Geolocation is not supported by this browser.";
   }
 }
-    
+
 function showPosition(position) {
-    var x = document.getElementById("demo");
-    x.innerText = "Latitude: " + position.coords.latitude + "\n"+
-    "Longitude: " + position.coords.longitude;
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
+
+    const pos = {"latitude": latitude, "longitude": longitude};
+    console.log(pos)
+    return pos;
 }
 
 function returnLocation() {
@@ -23,7 +21,6 @@ function returnLocation() {
   } else { 
     document.addEventListener('DOMContentLoaded', getLocation) 
   }
-  return {"latitude": latitude, "longitude": longitude}
 }
 
 returnLocation();
