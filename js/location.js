cddede8e8160b0388afd27f9bfebcc44b77143fd
Joli Holmes <holmesjoli@gmail.com>
2023-03-07 11:15:36 -0500

    organized flow of code

--- js/location.js
+++ js/location.js
@@ -1,14 +1,9 @@
-if (document.readyState !== 'loading') { 
-	setTimeout(onReady, 0); 
-} else { 
-	document.addEventListener('DOMContentLoaded', onReady) 
-} 
- 
+var x = document.getElementById("demo");
+let latitude, longitude;
+
 function onReady() { 
    getLocation();
 }
-
-var x = document.getElementById("demo");
 
 function getLocation() {
   if (navigator.geolocation) {
@@ -22,4 +17,12 @@ function showPosition(position) {
     var x = document.getElementById("demo");
     x.innerText = "Latitude: " + position.coords.latitude + "\n"+
     "Longitude: " + position.coords.longitude;
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+}
+
+if (document.readyState !== 'loading') { 
+	setTimeout(onReady, 0); 
+} else { 
+	document.addEventListener('DOMContentLoaded', onReady) 
 }
