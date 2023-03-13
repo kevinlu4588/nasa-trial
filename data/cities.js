f527a4fdd705b185ccc7c37720f9547e67e0c93d
Joli Holmes <holmesjoli@gmail.com>
2023-03-13 14:24:37 -0400

    modularized code

---
+++ data/cities.js
@@ -0,0 +1,464 @@
+export const cities = [
+    {
+      "city_ascii": "New York",
+      "state_name": "New York",
+      "lat": 40.6943,
+      "lng": -73.9249,
+      "population": 18972871
+    },
+    {
+      "city_ascii": "Los Angeles",
+      "state_name": "California",
+      "lat": 34.1141,
+      "lng": -118.4068,
+      "population": 12121244
+    },
+    {
+      "city_ascii": "Chicago",
+      "state_name": "Illinois",
+      "lat": 41.8375,
+      "lng": -87.6866,
+      "population": 8595181
+    },
+    {
+      "city_ascii": "Miami",
+      "state_name": "Florida",
+      "lat": 25.784,
+      "lng": -80.2101,
+      "population": 5711945
+    },
+    {
+      "city_ascii": "Dallas",
+      "state_name": "Texas",
+      "lat": 32.7935,
+      "lng": -96.7667,
+      "population": 5668165
+    },
+    {
+      "city_ascii": "Houston",
+      "state_name": "Texas",
+      "lat": 29.786,
+      "lng": -95.3885,
+      "population": 5650910
+    },
+    {
+      "city_ascii": "Philadelphia",
+      "state_name": "Pennsylvania",
+      "lat": 40.0077,
+      "lng": -75.1339,
+      "population": 5512873
+    },
+    {
+      "city_ascii": "Atlanta",
+      "state_name": "Georgia",
+      "lat": 33.7628,
+      "lng": -84.422,
+      "population": 5046555
+    },
+    {
+      "city_ascii": "Washington",
+      "state_name": "District of Columbia",
+      "lat": 38.9047,
+      "lng": -77.0163,
+      "population": 4810669
+    },
+    {
+      "city_ascii": "Boston",
+      "state_name": "Massachusetts",
+      "lat": 42.3188,
+      "lng": -71.0852,
+      "population": 4208580
+    },
+    {
+      "city_ascii": "Phoenix",
+      "state_name": "Arizona",
+      "lat": 33.5722,
+      "lng": -112.0892,
+      "population": 4047095
+    },
+    {
+      "city_ascii": "Detroit",
+      "state_name": "Michigan",
+      "lat": 42.3834,
+      "lng": -83.1024,
+      "population": 3522856
+    },
+    {
+      "city_ascii": "Seattle",
+      "state_name": "Washington",
+      "lat": 47.6211,
+      "lng": -122.3244,
+      "population": 3438221
+    },
+    {
+      "city_ascii": "San Francisco",
+      "state_name": "California",
+      "lat": 37.7558,
+      "lng": -122.4449,
+      "population": 3290197
+    },
+    {
+      "city_ascii": "San Diego",
+      "state_name": "California",
+      "lat": 32.8313,
+      "lng": -117.1222,
+      "population": 3084174
+    },
+    {
+      "city_ascii": "Minneapolis",
+      "state_name": "Minnesota",
+      "lat": 44.9635,
+      "lng": -93.2678,
+      "population": 2856952
+    },
+    {
+      "city_ascii": "Brooklyn",
+      "state_name": "New York",
+      "lat": 40.6501,
+      "lng": -73.9496,
+      "population": 2736074
+    },
+    {
+      "city_ascii": "Tampa",
+      "state_name": "Florida",
+      "lat": 27.9945,
+      "lng": -82.4447,
+      "population": 2683956
+    },
+    {
+      "city_ascii": "Denver",
+      "state_name": "Colorado",
+      "lat": 39.762,
+      "lng": -104.8758,
+      "population": 2650725
+    },
+    {
+      "city_ascii": "Queens",
+      "state_name": "New York",
+      "lat": 40.7498,
+      "lng": -73.7976,
+      "population": 2405464
+    },
+    {
+      "city_ascii": "Baltimore",
+      "state_name": "Maryland",
+      "lat": 39.3051,
+      "lng": -76.6144,
+      "population": 2205092
+    },
+    {
+      "city_ascii": "Las Vegas",
+      "state_name": "Nevada",
+      "lat": 36.2333,
+      "lng": -115.2654,
+      "population": 2150373
+    },
+    {
+      "city_ascii": "St. Louis",
+      "state_name": "Missouri",
+      "lat": 38.6359,
+      "lng": -90.2451,
+      "population": 2092481
+    },
+    {
+      "city_ascii": "Portland",
+      "state_name": "Oregon",
+      "lat": 45.5371,
+      "lng": -122.65,
+      "population": 2036875
+    },
+    {
+      "city_ascii": "Riverside",
+      "state_name": "California",
+      "lat": 33.9381,
+      "lng": -117.3949,
+      "population": 2022285
+    },
+    {
+      "city_ascii": "Orlando",
+      "state_name": "Florida",
+      "lat": 28.4773,
+      "lng": -81.337,
+      "population": 1927699
+    },
+    {
+      "city_ascii": "Sacramento",
+      "state_name": "California",
+      "lat": 38.5677,
+      "lng": -121.4685,
+      "population": 1924167
+    },
+    {
+      "city_ascii": "San Juan",
+      "state_name": "Puerto Rico",
+      "lat": 18.3985,
+      "lng": -66.061,
+      "population": 1915105
+    },
+    {
+      "city_ascii": "San Antonio",
+      "state_name": "Texas",
+      "lat": 29.4632,
+      "lng": -98.5238,
+      "population": 1910785
+    },
+    {
+      "city_ascii": "San Jose",
+      "state_name": "California",
+      "lat": 37.3012,
+      "lng": -121.848,
+      "population": 1729879
+    },
+    {
+      "city_ascii": "Pittsburgh",
+      "state_name": "Pennsylvania",
+      "lat": 40.4397,
+      "lng": -79.9763,
+      "population": 1720279
+    },
+    {
+      "city_ascii": "Cincinnati",
+      "state_name": "Ohio",
+      "lat": 39.1413,
+      "lng": -84.506,
+      "population": 1712287
+    },
+    {
+      "city_ascii": "Manhattan",
+      "state_name": "New York",
+      "lat": 40.7834,
+      "lng": -73.9662,
+      "population": 1694263
+    },
+    {
+      "city_ascii": "Cleveland",
+      "state_name": "Ohio",
+      "lat": 41.4764,
+      "lng": -81.6805,
+      "population": 1683059
+    },
+    {
+      "city_ascii": "Indianapolis",
+      "state_name": "Indiana",
+      "lat": 39.7771,
+      "lng": -86.1458,
+      "population": 1659305
+    },
+    {
+      "city_ascii": "Austin",
+      "state_name": "Texas",
+      "lat": 30.3005,
+      "lng": -97.7522,
+      "population": 1659251
+    },
+    {
+      "city_ascii": "Kansas City",
+      "state_name": "Missouri",
+      "lat": 39.1238,
+      "lng": -94.5541,
+      "population": 1644497
+    },
+    {
+      "city_ascii": "Columbus",
+      "state_name": "Ohio",
+      "lat": 39.9862,
+      "lng": -82.9855,
+      "population": 1556848
+    },
+    {
+      "city_ascii": "Charlotte",
+      "state_name": "North Carolina",
+      "lat": 35.2083,
+      "lng": -80.8303,
+      "population": 1516107
+    },
+    {
+      "city_ascii": "Virginia Beach",
+      "state_name": "Virginia",
+      "lat": 36.7335,
+      "lng": -76.0435,
+      "population": 1500764
+    },
+    {
+      "city_ascii": "Bronx",
+      "state_name": "New York",
+      "lat": 40.8501,
+      "lng": -73.8662,
+      "population": 1472654
+    },
+    {
+      "city_ascii": "Milwaukee",
+      "state_name": "Wisconsin",
+      "lat": 43.0642,
+      "lng": -87.9675,
+      "population": 1340981
+    },
+    {
+      "city_ascii": "Providence",
+      "state_name": "Rhode Island",
+      "lat": 41.823,
+      "lng": -71.4187,
+      "population": 1270149
+    },
+    {
+      "city_ascii": "Jacksonville",
+      "state_name": "Florida",
+      "lat": 30.3322,
+      "lng": -81.6749,
+      "population": 1220191
+    },
+    {
+      "city_ascii": "Salt Lake City",
+      "state_name": "Utah",
+      "lat": 40.7776,
+      "lng": -111.9311,
+      "population": 1135344
+    },
+    {
+      "city_ascii": "Nashville",
+      "state_name": "Tennessee",
+      "lat": 36.1715,
+      "lng": -86.7842,
+      "population": 1098486
+    },
+    {
+      "city_ascii": "Raleigh",
+      "state_name": "North Carolina",
+      "lat": 35.8324,
+      "lng": -78.6429,
+      "population": 1062018
+    },
+    {
+      "city_ascii": "Memphis",
+      "state_name": "Tennessee",
+      "lat": 35.1087,
+      "lng": -89.9663,
+      "population": 1034498
+    },
+    {
+      "city_ascii": "Louisville",
+      "state_name": "Kentucky",
+      "lat": 38.1663,
+      "lng": -85.6485,
+      "population": 1022630
+    },
+    {
+      "city_ascii": "Richmond",
+      "state_name": "Virginia",
+      "lat": 37.5295,
+      "lng": -77.4756,
+      "population": 1008069
+    },
+    {
+      "city_ascii": "Buffalo",
+      "state_name": "New York",
+      "lat": 42.9018,
+      "lng": -78.8487,
+      "population": 1004655
+    },
+    {
+      "city_ascii": "Oklahoma City",
+      "state_name": "Oklahoma",
+      "lat": 35.4676,
+      "lng": -97.5136,
+      "population": 994284
+    },
+    {
+      "city_ascii": "Bridgeport",
+      "state_name": "Connecticut",
+      "lat": 41.1918,
+      "lng": -73.1954,
+      "population": 975078
+    },
+    {
+      "city_ascii": "New Orleans",
+      "state_name": "Louisiana",
+      "lat": 30.0687,
+      "lng": -89.9288,
+      "population": 925443
+    },
+    {
+      "city_ascii": "Fort Worth",
+      "state_name": "Texas",
+      "lat": 32.7817,
+      "lng": -97.3474,
+      "population": 908469
+    },
+    {
+      "city_ascii": "Hartford",
+      "state_name": "Connecticut",
+      "lat": 41.7661,
+      "lng": -72.6834,
+      "population": 907046
+    },
+    {
+      "city_ascii": "Tucson",
+      "state_name": "Arizona",
+      "lat": 32.1541,
+      "lng": -110.8787,
+      "population": 875284
+    },
+    {
+      "city_ascii": "Honolulu",
+      "state_name": "Hawaii",
+      "lat": 21.3294,
+      "lng": -157.846,
+      "population": 835291
+    },
+    {
+      "city_ascii": "McAllen",
+      "state_name": "Texas",
+      "lat": 26.2252,
+      "lng": -98.2467,
+      "population": 809002
+    },
+    {
+      "city_ascii": "Omaha",
+      "state_name": "Nebraska",
+      "lat": 41.2627,
+      "lng": -96.0529,
+      "population": 806485
+    },
+    {
+      "city_ascii": "El Paso",
+      "state_name": "Texas",
+      "lat": 31.8476,
+      "lng": -106.43,
+      "population": 794344
+    },
+    {
+      "city_ascii": "Albuquerque",
+      "state_name": "New Mexico",
+      "lat": 35.1054,
+      "lng": -106.6465,
+      "population": 765693
+    },
+    {
+      "city_ascii": "Rochester",
+      "state_name": "New York",
+      "lat": 43.168,
+      "lng": -77.6162,
+      "population": 737309
+    },
+    {
+      "city_ascii": "Sarasota",
+      "state_name": "Florida",
+      "lat": 27.3387,
+      "lng": -82.5432,
+      "population": 727388
+    },
+    {
+      "city_ascii": "Fresno",
+      "state_name": "California",
+      "lat": 36.783,
+      "lng": -119.7939,
+      "population": 719558
+    },
+    {
+      "city_ascii": "Tulsa",
+      "state_name": "Oklahoma",
+      "lat": 36.1283,
+      "lng": -95.9042,
+      "population": 715983
+    }
+  ]
