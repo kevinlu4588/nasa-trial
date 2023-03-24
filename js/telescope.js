b58107cee76cf27def067c92c16e00989410cb1c
kevinlu4588 <kevinlu4588@gmail.com>
2023-03-24 11:34:21 -0400

    b;ah

---
+++ js/telescope.js
@@ -0,0 +1,48 @@
+const app = new PIXI.Application();
+
+document.getElementById("telescope").appendChild(app.view);
+
+//Adapted from https://pixijs.io/examples/#/masks/filter.js
+export function draw(imagePath = 'nasa_trial/assets/star_chart.png') {
+    // Inner radius of the circle
+    const radius = 100;
+    // The blur amount
+    const blurSize = 5;
+
+    PIXI.Assets.load(imagePath).then((stars) => {
+        const background = new PIXI.Sprite(stars);
+        app.stage.addChild(background);
+        background.width = app.screen.width;
+        background.height = app.screen.height;
+
+        const circle = new PIXI.Graphics()
+            .beginFill(0xFF0000)
+            .drawCircle(radius + blurSize, radius + blurSize, radius)
+            .endFill();
+        circle.filters = [new PIXI.filters.BlurFilter(blurSize)];
+
+        const bounds = new PIXI.Circle(0, 0, (radius + blurSize) * 2, (radius + blurSize) * 2);
+        const texture = app.renderer.generateTexture(circle, PIXI.SCALE_MODES.NEAREST, 1, bounds);
+        const focus = new PIXI.Sprite(texture);
+        background.mask = focus;
+
+        app.stage.addChild(focus);
+        focus.position.x = background.width/3;
+        focus.position.y= background.height/3;
+
+        app.stage.interactive = true;
+        app.stage.hitArea = app.screen;
+        app.stage.on('pointermove', (event) => {
+            focus.position.x = event.global.x - focus.width / 2;
+            focus.position.y = event.global.y - focus.height / 2;
+        });
+    });
+}
+
+// export function updateImage(imagePath) {
+
+//     PIXI.Assets.load(imagePath).then((stars) => {
+//         const background = new PIXI.Sprite(stars);
+//         app.stage.addChild(background);
+//     });
+// }
